28e5679a45c08086f6c07ca4f78e3075324cbf5a
Lisa Miller <lisayevonne@outlook.com>
2020-06-26 10:45:28 -0400

    added delete temp and controller

--- src/main/java/com/win/winblog/blogpost/BlogPostController.java
+++ src/main/java/com/win/winblog/blogpost/BlogPostController.java
@@ -79,4 +79,10 @@ public class BlogPostController {
         return "blogpost/result";
     }
 
+    @RequestMapping(value = "blogposts/delete/{id}")
+    public String deletePostById(@PathVariable Long id, BlogPost blogPost) {
+        blogPostRepository.deleteById(id);
+        return "blogpost/delete";
+    }
+
 }
